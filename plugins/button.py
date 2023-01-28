d5aa4550cebffc2d875178330702d7ca8f9dfa1e
fadhilabdat04 <98678420+fadhilabdat04@users.noreply.github.com>
2023-01-29 00:49:37 +0700

    Update button.py
    Signed-off-by: fadhilabdat04 <98678420+fadhilabdat04@users.noreply.github.com>

--- plugins/button.py
+++ plugins/button.py
@@ -45,8 +45,8 @@ def start_button(client):
                 InlineKeyboardButton(text="• ᴛᴇɴᴛᴀɴɢ sᴀʏᴀ •", callback_data="about"),
             ],
             [
-                InlineKeyboardButton(text="𝐂𝐇𝐀𝐍𝐍𝐄𝐋", url=client.invitelink),
-                InlineKeyboardButton(text="𝐂𝐇𝐀𝐍𝐍𝐄𝐋", url=client.invitelink2),
+                InlineKeyboardButton(text="CHANNEL I", url=client.invitelink),
+                InlineKeyboardButton(text="CHANNEL II", url=client.invitelink2),
             ],
             [InlineKeyboardButton(text="• ᴛᴜᴛᴜᴘ •", callback_data="close")],
         ]
